a297fccb95391fc74a230c23bf1395a8f3773f50
Byron Barahona <barahonabyron38@gmail.com>
2022-11-18 21:27:35 -0300

    Avance Codigo

--- ProyectoJava3/src/Controlador/Conexion.java
+++ ProyectoJava3/src/Controlador/Conexion.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
 
-/**
- *
- * @author Alumnos
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
+ private String user = "root";
+ private String pass = "root";
+ private String srv = "jdbc:mysql://localhost/ejercicioJava";
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getSrv() {
+        return srv;
+    }
+
+    public void setSrv(String srv) {
+        this.srv = srv;
+    }
+
+    public Conexion(String user, String pass, String srv) {
+        this.user = user;
+        this.pass = pass;
+        this.srv = srv;
+    }
     
+    
+ 
+ 
+ public boolean conexion(){
+     boolean resultado = false;
+     try{
+         Class.forName("com.mysql.jdbc.Driver");
+         Connection con = DriverManager.getConnection(srv, user,pass);
+         
+     }catch(Exception e) {
+            e.printStackTrace();
+     }
+     return false;
+ }
+ 
 }
